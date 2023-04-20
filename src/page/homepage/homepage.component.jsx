e360680cff777133f3cac2b49bb0cc26229e47e6
Ekpoto Sammuel <ekpotosammuel1@gmail.com>
2023-04-20 22:32:18 +0100

    fixing menu iteams

---
+++ src/page/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './homepage.styles.scss'
+import Directory from "../../component/directory/directory.componet";
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+    
+);
+
+export default HomePage;
